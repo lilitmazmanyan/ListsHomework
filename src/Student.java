eac2099a036cbbf51f9e4ec7e03718c13ccfd6e5
Lilit <lilit.mazmanyan1@gmail.com>
2020-01-21 20:10:32 +0400

    Added fields in Student class

--- src/Student.java
+++ src/Student.java
@@ -2,5 +2,24 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 public class Student implements Comparable<Student> {
+    String firstName;
+    String lastName;
+    int age;
+
+    public Student(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " is " + age + " years old.";
+    }
+
+    @Override
+    public int compareTo(Student student) {
+        return this.firstName.compareTo(student.firstName);
+    }
 
 }
