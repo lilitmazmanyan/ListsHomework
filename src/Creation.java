5d26b704cf48b30fef90bb5f0b261bc5227f5da1
Lilit <lilit.mazmanyan1@gmail.com>
2020-01-22 01:44:29 +0400

    Modified Creation file

--- src/Creation.java
+++ src/Creation.java
@@ -2,7 +2,19 @@ import java.util.*;
 
 public class Creation {
     public static void main(String[] args) {
-        List<Student> studnetsList = new ArrayList<>();
+        MyArrayList<Student> listToTest = new MyArrayList<>();
+        listToTest.add(new Student("Levon", "Mazmanyan", 43));
+        listToTest.add(new Student("Zara", "Ordyan", 42));
+        listToTest.add(new Student("Nune", "Mazmanyan", 15));
+        System.out.println("Current size is " + listToTest.size());
+        Student s1 = new Student("James", "Bond", 43);
+        Student s2 = new Student("Mark", "Levi", 59);
+        listToTest.add(s1);
+        listToTest.add(s2);
+        System.out.println("Size after adding 2 students " + listToTest.size());
+        System.out.println(listToTest.get(3));
+
+        ArrayList<Student> studnetsList = new ArrayList();
         studnetsList.add(new Student("Lilit", "Mazmanyan", 19));
         studnetsList.add(new Student("Koj", "Polorchyan", 20));
         studnetsList.add(new Student("Mar", "Sargsyan", 36));
@@ -53,6 +65,6 @@ public class Creation {
             System.out.println(s);
         }
         System.out.println("------------------Break-Here------------------");
-
+//
     }
 }
